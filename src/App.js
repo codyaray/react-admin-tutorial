340ebc41b0bfd030114d0de6ad4acd2b367651de
Cody A. Ray <cody@confluent.io>
2020-09-06 08:03:29 -0500

    Finish react-admin tutorial
    Signed-off-by: Cody A. Ray <cody@confluent.io>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './components/UserList';
+import { PostList, PostCreate, PostEdit } from "./components/posts";
+import jsonServerProvider from 'ra-data-json-server';
+import createHistory from 'history/createBrowserHistory';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './components/dashboard';
+import authProvider from './authProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const history = createHistory();
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} history={history}>
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+    </Admin>
+);
 
 export default App;
